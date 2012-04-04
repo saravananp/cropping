0e9f45029dc93b24974a5ac238a0cb87b6c72814
Saravanan P <saravanan.p@shriramits.com>
2012-04-04 18:22:24 +0530

    a

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,6 @@ class PostsController < ApplicationController
   # GET /posts.xml
   def index
     @posts = Post.all
-
     respond_to do |format|
       format.html # index.html.erb
       format.xml  { render :xml => @posts }
